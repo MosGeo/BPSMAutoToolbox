90576caa8b8875307935189b870b8ed8bc713274
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2019-02-05 10:22:58 -0800

    Handle loading with different PM versions

--- PMScripts/loadOutputOverlays.m
+++ PMScripts/loadOutputOverlays.m
@@ -1,15 +1,20 @@
-function [data, layerNames] = loadOutputOverlays(modelName, nDim, PM, overlayNumbers)
+function [data, layerNames, unit] = loadOutputOverlays(modelName, nDim, PM, overlayNumbers)
 
 scriptFolder = fullfile(fileparts(fileparts(PM.PMDirectory)), 'scripts');
 
 data = []; 
 layerNames=[];
 for iLayer = 1:numel(overlayNumbers)
-    [cmdout, status] = PM.runScript(modelName, nDim, 'demo_opensim_output_3rd_party_format', num2str(overlayNumbers(iLayer)), false, false, scriptFolder); 
-    [id, value, layer, unit] = readDemoScriptOutput('demo_1.txt');
-    delete demo_1.txt
+    [cmdout, status] = PM.runScript(modelName, nDim, 'demo_opensim_output_3rd_party_format', num2str(overlayNumbers(iLayer)), false, false, scriptFolder);
+    if PM.version >= 2018
+        outputFileName =  fullfile(PM.getModelFolder(modelName, nDim),'out','demo_1.txt') ;
+    else
+        outputFileName = 'demo_1.txt';
+    end
+    [id, value, layer, unit] = readDemoScriptOutput(outputFileName, true);  
     data = [data, value];
     layerNames{iLayer} = layer;
+    units{iLayer} = unit;
 end
 
 end
